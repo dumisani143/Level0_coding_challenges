8cfd6464610df3ee08fd2d477b99653582372180
dumisani143 <dp.lukhele@gmail.com>
2021-08-17 17:09:28 +0200

    send feedback changes

--- task0.10.js
+++ task0.10.js
@@ -1,19 +1,16 @@
-function commonCharactors(string1, string2) {
-   
-    var str1 = string1.toLowerCase();
-    var str2 = string2.toLowerCase();
-    var result = "";
-
-    //finds matches between strings and adds to result
-    for (var a = 0; a < str1.length; a++) {
-      for (var b = 0; b < str2.length; b++) {
-        if (str1[a] === str2[b]) {
-          result += str1[a]+",";
-        }
-      }
-      
-    }
- console.log(result);
-  }
   
-  commonCharactors("house", "computers");
+function matchChar(str,str1) {
+  let strVowels = [];
+
+  //finds matching char in string
+  for (let i = 0; i < str.length; i++) {
+      if (str.includes(str1[i])) {
+          if (!strVowels.includes(str[i].toLowerCase())) {
+          strVowels.push(str1[i].toLowerCase());
+          }    
+      }
+  }
+  console.log(strVowels);
+}
+  
+  matchChar("houseses", "computers");
